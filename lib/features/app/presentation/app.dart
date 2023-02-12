d95fa104f9b893ba5650a4135e1fc65f2b0ae029
Unsignedmind <unsignedmind.dev@gmail.com>
2023-02-12 15:48:31 +0100

    official localization

--- lib/features/app/presentation/app.dart
+++ lib/features/app/presentation/app.dart
@@ -3,6 +3,7 @@ import 'package:cc_threehours/features/app/domain/screen.model.dart';
 import 'package:cc_threehours/features/home/presentation/home.dart';
 import 'package:cc_threehours/features/messages/presentation/messages_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -12,83 +13,100 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: AppInner());
+  }
+}
+
+class AppInner extends StatefulWidget {
+  const AppInner({super.key});
+
+  @override
+  State<AppInner> createState() => _AppInnerState();
+}
+
+class _AppInnerState extends State<AppInner> {
   int _currentIndex = 0;
-  static final AppScreens screens = AppScreens([
+
+  final AppScreens screens = AppScreens([
     Screen('Home', const HomePage()),
     Screen('Messages', const MessagesPage()),
   ]);
+
   final _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(80.0),
-          child: Container(
-            height: 120,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: <Color>[
-                  Colors.redAccent,
-                  Colors.orangeAccent,
-                ],
-              ),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: Container(
+          height: 120,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: <Color>[
+                Colors.redAccent,
+                Colors.orangeAccent,
+              ],
             ),
-            child: SafeArea(
-              child: Center(
-                child: ListTile(
-                  title: Text(
-                    screens.getTitle(_currentIndex),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(
-                      Icons.search,
-                      size: 20,
-                    ),
+          ),
+          child: SafeArea(
+            child: Center(
+              child: ListTile(
+                title: Text(
+                  screens.getTitle(_currentIndex),
+                  style: const TextStyle(
                     color: Colors.white,
-                    onPressed: () {},
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
+                ),
+                trailing: IconButton(
+                  icon: const Icon(
+                    Icons.search,
+                    size: 20,
+                  ),
+                  color: Colors.white,
+                  onPressed: () {},
                 ),
               ),
             ),
           ),
         ),
-        body: PageView(
-          controller: _pageController,
-          onPageChanged: (newIndex) {
-            setState(() {
-              _currentIndex = newIndex;
-            });
-          },
-          children: screens.getWidgets(),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.message),
-              label: 'Messages',
-            ),
-          ],
-          currentIndex: _currentIndex,
-          onTap: (int index) {
-            _pageController.animateToPage(
-              index,
-              duration: const Duration(milliseconds: 500),
-              curve: Curves.ease,
-            );
-          },
-        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        children: screens.getWidgets(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: AppLocalizations.of(context)!.home,
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Messages',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.ease,
+          );
+        },
       ),
     );
   }
