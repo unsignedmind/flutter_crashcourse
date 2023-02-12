4b19c2d66a744284052a79e36df568691444451c
Unsignedmind <unsignedmind.dev@gmail.com>
2023-02-12 21:18:24 +0100

    implement drawer

--- lib/features/app/presentation/app.dart
+++ lib/features/app/presentation/app.dart
@@ -33,16 +33,56 @@ class AppInner extends StatefulWidget {
 class _AppInnerState extends State<AppInner> {
   int _currentIndex = 0;
 
-  final AppScreens screens = AppScreens([
-    Screen('Home', const HomePage()),
-    Screen('Messages', const MessagesPage()),
-  ]);
-
   final _pageController = PageController(initialPage: 0);
+
+  void changePage(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.ease,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    AppScreens screens = AppScreens([
+      Screen(AppLocalizations.of(context)!.home, const HomePage()),
+      Screen(AppLocalizations.of(context)!.messages, const MessagesPage()),
+    ]);
+
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Colors.redAccent,
+                    Colors.orangeAccent,
+                  ],
+                ),
+              ),
+              child: Text(screens.getTitle(_currentIndex)),
+            ),
+            ListTile(
+              title: Text(AppLocalizations.of(context)!.home),
+              onTap: () {
+                changePage(0);
+                Navigator.of(context).pop(this);
+              },
+            ),
+            ListTile(
+              title: Text(AppLocalizations.of(context)!.messages),
+              onTap: () {
+                changePage(1);
+                Navigator.of(context).pop(this);
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(80.0),
         child: Container(
@@ -58,13 +98,29 @@ class _AppInnerState extends State<AppInner> {
           child: SafeArea(
             child: Center(
               child: ListTile(
-                title: Text(
-                  screens.getTitle(_currentIndex),
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+                title: Row(
+                  children: [
+                    Builder(builder: (context) {
+                      return IconButton(
+                        icon: const Icon(Icons.menu),
+                        color: Colors.white,
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                      );
+                    }),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      screens.getTitle(_currentIndex),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
                 trailing: IconButton(
                   icon: const Icon(
@@ -94,18 +150,14 @@ class _AppInnerState extends State<AppInner> {
             icon: const Icon(Icons.home),
             label: AppLocalizations.of(context)!.home,
           ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: 'Messages',
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.message),
+            label: AppLocalizations.of(context)!.messages,
           ),
         ],
         currentIndex: _currentIndex,
         onTap: (int index) {
-          _pageController.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.ease,
-          );
+          changePage(index);
         },
       ),
     );
